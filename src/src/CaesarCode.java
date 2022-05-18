7f096bd3d879a4369ebe1099272fc4d33d9ae064
Maria Safonova <safonovama@mail.ru>
2022-05-18 22:29:33 +0300

    Добавлены методы зашифровки и расшифровки

---
+++ src/src/CaesarCode.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CaesarCode {
+    private static ArrayList<Character> alphabet = new ArrayList<Character>(Arrays.asList('А', 'Б', 'В', 'Г', 'Д',
+                                                                                          'Е', 'Ё', 'Ж', 'З', 'И',
+                                                                                          'Й', 'К', 'Л', 'М', 'Н',
+                                                                                          'О', 'П', 'Р', 'С', 'Т',
+                                                                                          'У', 'Ф', 'Х', 'Ц', 'Ч',
+                                                                                          'Ш', 'Щ', 'Ъ', 'Ы', 'Ь',
+                                                                                          'Э', 'Ю', 'Я', 'а', 'б',
+                                                                                          'в', 'г', 'д', 'е', 'ё',
+                                                                                          'ж', 'з', 'и', 'й', 'к',
+                                                                                          'л', 'м', 'н', 'о', 'п',
+                                                                                          'р', 'с', 'т', 'у', 'ф',
+                                                                                          'х', 'ц', 'ч', 'ш', 'щ',
+                                                                                          'ъ', 'ы', 'ь', 'э', 'ю',
+                                                                                          'я', '.', ',', '!', '?',
+                                                                                          ':', '-', ' ', '"'));
+    public static char getSymbol (int key) {
+        if (key > alphabet.size() - 1) {
+            key = key - alphabet.size();
+        } else if (key < 0) {
+            key = key + alphabet.size();
+        }
+        return alphabet.get(key);
+    }
+    public static boolean contains (char symbol) {
+        return alphabet.contains(symbol);
+    }
+    public static int indexOf (char symbol) {
+        return alphabet.indexOf(symbol);
+    }
+}
---
+++ src/src/Interface.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Interface {
+    public static void startProgram () {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Выберите действие:");
+        System.out.println("Для шифровки/расшифровки текста шифром Цезаря, введите: Caesar");
+        System.out.println("Для расшифровки текста методом Brute Force, введите: BF");
+        System.out.println("Поле ввода:");
+        while (true) {
+            String choice = console.nextLine();
+            if (choice.equalsIgnoreCase("Caesar")) {
+                caesar();
+                break;
+            } else  if (choice.equalsIgnoreCase("BF")){
+                bruteForce();
+                break;
+            } else {
+                System.out.println("Выберите одно из предложенных действий");
+            }
+        }
+    }
+    public static void caesar () {
+        Scanner console = new Scanner(System.in);
+            System.out.println("Если Вы хотите зашифровать текст, введите \"Encrypt\"");
+            System.out.println("Если Вы хотите расшифровать текст, введите \"Decrypt\"");
+            System.out.println("Поле ввода:" );
+        while (true) {
+            String choice = console.nextLine();
+            if (choice.equalsIgnoreCase("Encrypt")) {
+                System.out.println("Введите путь к файлу в формате: \"C:\\User\\...\\text.txt\"");
+                Reader.encrypt();
+                System.out.println("Вы можете посмотреть результат в файле \"Text/Result.txt\"");
+                break;
+            } else if (choice.equalsIgnoreCase("Decrypt")) {
+                System.out.println("Введите путь к файлу в формате: \"C:\\User\\...\\text.txt\"");
+                Reader.decrypt();
+                System.out.println("Вы можете посмотреть результат в файле \"Text/Result.txt\"");
+                break;
+            } else {
+                System.out.println("Выберите одно из предложенных действий");
+            }
+        }
+    }
+    public static void bruteForce () {
+    }
+}
